a735a4fac6bc406f79dcfbecf05917e7ec6d6991
Cesare Ferrari <cesare@maryandferrari.com>
2015-12-28 11:55:49 -0500

    Refactor page helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,17 +1,23 @@
 module PagesHelper
   def side_links_for(page)
     content_tag :ul do
-
       if page.experts?
-        Expert::CATEGORIES.collect do |name|
-          concat(content_tag(:li, link_to(name, experts_path(category: name.parameterize.underscore))))
-        end
+        links_for_experts
       else
-        page.side_nav.collect do |c|
-          concat(content_tag(:li, link_to(c.title, page_path(c.slug))))
-        end
+        links_for_page(page)
       end
+    end
+  end
 
+  def links_for_experts
+    Expert::CATEGORIES.map do |c|
+      concat(content_tag(:li, link_to(c, experts_path(category: c.parameterize.underscore))))
+    end
+  end
+
+  def links_for_page(page)
+    page.side_nav.map do |p|
+      concat(content_tag(:li, link_to(p.title, page_path(p.slug))))
     end
   end
 end
