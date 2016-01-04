e4fb738c4b5e7d8217d53d925111ffb445c75c30
Cesare Ferrari <cesare@maryandferrari.com>
2016-01-04 06:05:07 -0500

    Make expert links active depending on the category

--- app/helpers/experts_helper.rb
+++ app/helpers/experts_helper.rb
@@ -1,2 +1,24 @@
 module ExpertsHelper
+  def side_links_for_experts(category=nil)
+    links = []
+    Expert::CATEGORIES.each do |key, value|
+      if category && category == value
+        links << link_to(value, experts_path(category: key.to_s), class: "side-active")
+      else
+        links << link_to(value, experts_path(category: key.to_s))
+      end
+    end
+
+    content_tag :ul do
+      links.each do |link|
+        concat(content_tag(:li, link))
+      end
+    end
+  end
+
+  def links_for_experts
+    Expert::CATEGORIES.each do |key, value|
+      concat(content_tag(:li, link_to(value, experts_path(category: key.to_s))))
+    end
+  end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -9,23 +9,7 @@ module PagesHelper
     end
   end
 
-  def side_links_for_experts
-    content_tag :ul do
-      links_for_experts
-    end
-  end
 
-  def links_for_experts
-    Expert::CATEGORIES.each do |key, value|
-      concat(content_tag(:li, link_to(value, experts_path(category: key.to_s))))
-    end
-  end
-
-#  def links_for_page(page)
-#    page.side_nav.each do |p|
-#      concat(content_tag(:li, link_to(p.title, page_path(p.slug))))
-#    end
-#  end
 
   def links_for_page(page)
     page.side_nav.each do |p|
