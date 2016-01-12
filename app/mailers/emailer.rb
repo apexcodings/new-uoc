50cba620225cabfbc5f5da11026ccbde535fe01a
Cesare Ferrari <cesare@maryandferrari.com>
2016-01-12 13:16:34 -0500

    Add emailer for contact

---
+++ app/mailers/emailer.rb
@@ -0,0 +1,15 @@
+class Emailer < ApplicationMailer
+
+  def contact_email(contact)
+    @contact = contact
+    mail(to: "cesare@maryandferrari.com",
+       subject: "Contact form submission from UOC website")
+  end
+
+  # to do
+  def appointment_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
