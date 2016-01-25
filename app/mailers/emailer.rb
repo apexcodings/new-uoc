a3af88ceac1ea24f18461b95f0eb69256ae3f6b5
Cesare Ferrari <cesare@maryandferrari.com>
2016-01-24 21:09:41 -0500

    Send form emails

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -3,32 +3,32 @@ class Emailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     mail( from:     contact.email,
-          to:       "cesare@maryandferrari.com",
-          #to:       "info@uoc.com"
+          #to:       "cesare@maryandferrari.com",
+          to:       "info@uoc.com",
           subject:  "Contact form submission from UOC website")
   end
 
   def review_email(review)
     @review = review
     mail( from:     review.email,
-          to:       "cesare@maryandferrari.com",
-          #to:       "info@uoc.com"
+          #to:       "cesare@maryandferrari.com",
+          to:       "info@uoc.com",
           subject:  "Patient Review form submission from UOC website")
   end
 
   def appointment_email(appointment)
     @appointment = appointment
     mail( from:     appointment.requestor_email,
-          to:       "cesare@maryandferrari.com",
-          #to:       "appts@uoc.com"
+          #to:       "cesare@maryandferrari.com",
+          to:       "appts@uoc.com",
           subject:  "New Appointment Request from UOC website")
   end
 
   def referral_email(referral)
     @referral = referral
     mail( from:     referral.email,
-          to:       "cesare@maryandferrari.com",
-          #to:       "appts@uoc.com"
+          #to:       "cesare@maryandferrari.com",
+          to:       "appts@uoc.com",
           subject:  "New Referral from UOC website")
   end
 end
