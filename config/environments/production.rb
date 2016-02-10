26efe1c5cd47dec78b9a91c21c1a9e88b3e3192b
Cesare Ferrari <cesare@maryandferrari.com>
2016-02-10 11:56:05 -0500

    Change rewrite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
 
   # URL rewrite
   config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301 %r{.*}, 'http://www.uoc.com$&', :if => Proc.new {|rack_env|
+    r301 %r{.*}, 'https://www.uoc.com$&', :if => Proc.new {|rack_env|
       rack_env['SERVER_NAME'] != 'www.uoc.com'
     }
   end
