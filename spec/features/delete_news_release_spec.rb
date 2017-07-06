927934d810ac17c74c674c96d23dff2a8af2bfaf
Cesare Ferrari <cesare@maryandferrari.com>
2017-07-06 15:04:51 -0400

    Remove news release from Show page

--- spec/features/delete_news_release_spec.rb
+++ spec/features/delete_news_release_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "Deleting a News Release" do
     sign_in(admin)
   end
 
-  it 'removes the News release from the database' do
+  it 'removes the News release from the listing' do
     visit admin_news_releases_url
 
     within "#news-#{release1.id}" do
@@ -19,6 +19,19 @@ RSpec.describe "Deleting a News Release" do
       click_link("Remove")
       }.to change(NewsRelease, :count)
     end
+
+    expect(current_path).to eq(admin_news_releases_path)
+    expect(page).to have_text('News Release successfully removed!')
+    expect(page).not_to have_text('News to remove')
+    expect(page).to have_text('News not deleted')
+  end
+
+  it 'removes the News from the show page' do
+    visit news_release_url(release1)
+
+    expect {
+    click_link("Remove")
+    }.to change(NewsRelease, :count)
 
     expect(current_path).to eq(admin_news_releases_path)
     expect(page).to have_text('News Release successfully removed!')
